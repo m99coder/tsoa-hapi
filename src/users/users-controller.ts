8225ae5aa4eaa1fbd68f9d9b6ae36ab41eeab1f1
Marco Lehmann <m99@posteo.de>
2021-12-07 20:41:06 +0100

    Add error type

--- src/users/users-controller.ts
+++ src/users/users-controller.ts
@@ -5,11 +5,17 @@ import {
   Path,
   Post,
   Query,
+  Response,
   Route,
   SuccessResponse,
 } from '@tsoa/runtime'
 import { User } from './users'
 import { UserCreationParams, UsersService } from './users-service'
+
+interface ValidateErrorJSON {
+  message: 'Validation failed'
+  details: { [name: string]: unknown }
+}
 
 @Route('users')
 export class UsersController extends Controller {
@@ -21,6 +27,7 @@ export class UsersController extends Controller {
     return new UsersService().get(userId, name)
   }
 
+  @Response<ValidateErrorJSON>(422, 'Validation failed') // Reflected in OpenAPI spec
   @SuccessResponse('201', 'Created') // Custom success response
   @Post()
   public async createUser(
